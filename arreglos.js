eb0637d74b97e1dce6d15c3a697822b807c562a1
alci19 <bytonalci@gmail.com>
2026-05-08 12:45:50 -0500

    Agregamos pintar y eliminar derecha

--- arreglos.js
+++ arreglos.js
@@ -20,22 +20,56 @@ function pintarArregloIzquierdo() {
     let tablaIzquierda = document.getElementById("tablaIzquierda");
     let contenidoIzquierda = "";
     for (let i = 0; i < arregloIzquierdo.length; i++) {
-        contenidoIzquierda +=`
-        <tr> 
+        contenidoIzquierda += `
+        <tr>
             <td>${arregloIzquierdo[i]}</td>
-            <td>
-                <button onclick="eliminarIzquierdo(${i})" class="btn-eliminar">Eliminar</button>
-            </td>
-            <td>
-                <button onclick="moverDerecha(${i})" class="btn-mover">➜</button>
-            </td>
-        </tr>`;
 
+            <td>
+                <button onclick="eliminarIzquierdo(${i})" class="btn-eliminar">
+                    Eliminar
+                </button>
+            </td>
+
+            <td>
+                <button onclick="moverDerecha(${i})" class="btn-mover">
+                    ➜
+                </button>
+            </td>
+        </tr>
+        `;
     }
     tablaIzquierda.innerHTML = contenidoIzquierda;
 }
 
 function eliminarIzquierdo(indice) {
+    //Debug para eliminar 
+    //console.log("Eliminando elemento en índice: " + indice);
+    //console.log( arregloIzquierdo);
     arregloIzquierdo.splice(indice, 1);
     pintarArregloIzquierdo();
+}
+
+function pintarArregloDerecho() {
+    let tablaDerecha = document.getElementById("tablaDerecha");
+    let contenidoDerecha = "";
+    for (let i = 0; i < arregloDerecho.length; i++) {
+        contenidoDerecha += `
+        <tr>
+            <td>${arregloDerecho[i]}</td>
+
+            <td>
+                <button onclick="eliminarDerecho(${i})" class="btn-eliminar">
+                    Eliminar
+                </button>
+            </td>
+
+            <td>
+                <button onclick="moverIzquierda(${i})" class="btn-mover">
+                    ⬅
+                </button>
+            </td>
+        </tr>
+        `;
+    }
+    tablaDerecha.innerHTML = contenidoDerecha;
 }
